9df59a8f85e2059e4a79cb86c93ac785db564652
Laurenz Maslo <laurenzmaslo@gmail.com>
2026-03-05 12:59:38 +0100

    initial commit

---
+++ src/main/java/de/lm/mybriefmarkensammlung/MybriefmarkensammlungApplication.java
@@ -0,0 +1,13 @@
+package de.lm.mybriefmarkensammlung;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MybriefmarkensammlungApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MybriefmarkensammlungApplication.class, args);
+	}
+
+}
