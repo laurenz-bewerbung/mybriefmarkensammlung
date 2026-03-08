de152393ba199c4620c7ac17934791dc7a4916ef
Laurenz Maslo <laurenzmaslo@gmail.com>
2026-03-08 13:38:23 +0100

    added details page for single collection

--- src/main/java/de/lm/mybriefmarkensammlung/controller/CollectionController.java
+++ src/main/java/de/lm/mybriefmarkensammlung/controller/CollectionController.java
@@ -4,8 +4,10 @@ import de.lm.mybriefmarkensammlung.service.CollectionService;
 import de.lm.mybriefmarkensammlung.service.ImageService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
@@ -28,6 +30,12 @@ public class CollectionController {
     public String overview(Model model) {
         model.addAttribute("collections", collectionService.getCollections());
         return "sammlungen/overview";
+    }
+
+    @GetMapping("/sammlungen/{id}")
+    public String details(Model model, @PathVariable("id") Long id) {
+        model.addAttribute("collection", collectionService.getCollection(id));
+        return "sammlungen/details";
     }
 
     @GetMapping("/sammlungen/add")
--- src/main/java/de/lm/mybriefmarkensammlung/service/CollectionService.java
+++ src/main/java/de/lm/mybriefmarkensammlung/service/CollectionService.java
@@ -12,6 +12,7 @@ import org.springframework.web.multipart.MultipartFile;
 import java.io.IOException;
 import java.util.HashSet;
 import java.util.List;
+import java.util.Optional;
 import java.util.Set;
 
 @Service
@@ -33,6 +34,16 @@ public class CollectionService {
 
         Collection collection = new Collection(category, description, images);
         collectionRepository.save(collection);
+    }
+
+    public Collection getCollection(Long id) {
+        Optional<Collection> optCollection = collectionRepository.findById(id);
+
+        if(optCollection.isEmpty()) {
+            // todo: empty Collection
+        }
+
+        return optCollection.get();
     }
 
     public List<Collection> getCollections() {
