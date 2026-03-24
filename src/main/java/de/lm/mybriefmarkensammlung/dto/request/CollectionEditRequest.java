10441a7b169242205accf7b61b83215b3197760a
Laurenz Maslo <laurenzmaslo@gmail.com>
2026-03-24 12:19:58 +0100

    added CollectionEditRequest DTO

---
+++ src/main/java/de/lm/mybriefmarkensammlung/dto/request/CollectionEditRequest.java
@@ -0,0 +1,59 @@
+package de.lm.mybriefmarkensammlung.dto.request;
+
+import de.lm.mybriefmarkensammlung.domain.model.ExhibitionClass;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
+public class CollectionEditRequest {
+
+    @NotBlank(message = "Gebe einen Titel an.")
+    private String title;
+    @NotNull(message = "Wähle eine Kategorie aus.")
+    private Long category;
+
+    // todo: add or delete images
+
+    private String description;
+    private boolean isExhibition;
+    private ExhibitionClass exhibitionClass;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Long getCategory() {
+        return category;
+    }
+
+    public void setCategory(Long category) {
+        this.category = category;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public ExhibitionClass getExhibitionClass() {
+        return exhibitionClass;
+    }
+
+    public void setExhibitionClass(ExhibitionClass exhibitionClass) {
+        this.exhibitionClass = exhibitionClass;
+    }
+
+    public boolean getIsExhibition() {
+        return isExhibition;
+    }
+
+    public void setIsExhibition(boolean exhibition) {
+        isExhibition = exhibition;
+    }
+}
