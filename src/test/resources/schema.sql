cea94b88c97372e612b37c9669e1e318aa0f0c78
Laurenz Maslo <laurenzmaslo@gmail.com>
2026-04-09 13:39:48 +0200

    changed test setup to use postgre db instead of h2

---
+++ src/test/resources/schema.sql
@@ -0,0 +1,47 @@
+-- DROP TABLE IF EXISTS "role" CASCADE;
+-- DROP TABLE IF EXISTS "user" CASCADE;
+-- DROP TABLE IF EXISTS category CASCADE;
+-- DROP TABLE IF EXISTS collection CASCADE;
+-- DROP TABLE IF EXISTS image CASCADE;
+-- DROP TABLE IF EXISTS collection_image CASCADE;
+
+CREATE TABLE IF NOT EXISTS "role" (
+                                      id SERIAL PRIMARY KEY,
+                                      authority VARCHAR(50) NOT NULL UNIQUE
+    );
+
+CREATE TABLE IF NOT EXISTS "user" (
+                                      id SERIAL PRIMARY KEY,
+                                      username VARCHAR(255) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL,
+    role_id BIGINT NOT NULL
+    );
+
+CREATE TABLE IF NOT EXISTS category (
+                                        id SERIAL PRIMARY KEY,
+                                        category VARCHAR(255),
+    parent_id BIGINT
+    );
+
+CREATE TABLE IF NOT EXISTS collection (
+                                          id SERIAL PRIMARY KEY,
+                                          title VARCHAR(255),
+    category_id BIGINT REFERENCES category(id) ON DELETE CASCADE,
+    description TEXT,
+    is_exhibition BOOLEAN,
+    exhibition_class VARCHAR(255),
+    user_id BIGINT REFERENCES "user"(id) on DELETE CASCADE
+    );
+
+CREATE TABLE IF NOT EXISTS image (
+                                     id SERIAL PRIMARY KEY,
+                                     filename VARCHAR(255),
+    content BYTEA
+    );
+
+CREATE TABLE IF NOT EXISTS collection_image (
+                                                collection_id BIGINT REFERENCES collection(id) ON DELETE CASCADE,
+    image_id BIGINT REFERENCES image(id) ON DELETE CASCADE,
+    order_id INT,
+    PRIMARY KEY (collection_id, image_id)
+    );
